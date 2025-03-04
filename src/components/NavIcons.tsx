20fa8da7da38d86b0e3a2131222cb3f533064577
anjumolgeorge <georgeanjumol782@gmail.com>
2025-03-04 13:26:50 +0530

    edit

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,10 +1,49 @@
 "use client"
-import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 const NavIcons = () => {
+    const [isProfileOpen, setIsProfileOpen] = useState(false)
+    const [isCartOpen, setIsCartOpen] = useState(false)
+
+    const router = useRouter()
+    // TEMPORARY
+    const isLoggedIn = false
+
+    const handleProfile = () => {
+        if(!isLoggedIn) {
+         router.push("/login")
+        }
+        setIsProfileOpen((prev) => !prev)
+    }
   return (
-    <div>
-      Navicons
+    <div className=' flex items-center gap-4 xl:gap-6'>
+    <img src="/user.png"
+      alt=""
+      width={22}
+      height={22} 
+      className='cursor-pointer'
+      onClick={handleProfile} />
+
+      {isProfileOpen && 
+      <div className=' absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+        <Link href="/">Profile</Link>
+        <div className=' mt-2 cursor-pointer'>Logout</div>
+        </div>}
+
+    <img src="/bell.png" 
+     alt="" 
+     width={22} 
+     height={22} 
+     className='cursor-pointer' />
+
+     <img src="/online-shopping.png" 
+     alt="" 
+     width={22} 
+     height={22} 
+     className='cursor-pointer' />
+     
     </div>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,32 @@
 "use client"
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const SearchBar = () => {
+    const router = useRouter();
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+         e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const name = formData.get("name") as string;
+
+        if (name){
+           router.push(`/list?name=${name}`)
+        }
+    }
   return (
-    <form className=' flex justify-between gap-4 bg-gray-200 p-2 rounded-md'>
-     <input type='text' placeholder='Search'/>
-     <button className=' cursor-pointer'>
-        <img src="/search.png" alt=""
-        width={16}
-        height={16} />
+    <form className=' flex justify-between gap-4 bg-gray-200 p-2 rounded-md'
+    onSubmit={handleSearch}>
+     <input
+      type='text' 
+      name='name'
+      placeholder='Search' 
+      className=' bg-transparent outline-none'
+      />
+     <button
+      className=' cursor-pointer'>
+      <img src="/search.png" alt=""
+      width={16}
+      height={16} />
      </button>
     </form>
   );
