c08a8923dc4e6b710af1da3c93796916bee6eb9b
anjumolgeorge <georgeanjumol782@gmail.com>
2025-03-10 10:50:32 +0530

    edit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,16 @@ import NavIcons from './NavIcons';
 
 const Navbar = () => {
   return (
-    <div className='bg-slate-500 h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
+    <div className='bg-slate-500 h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative'>
          {/* MOBILE */}
-        <div className=' h-full flex items-center justify-between'>
+        <div className=' h-full flex items-center justify-between md:hidden'>
       <Link href="/">
       <div className=' text-2xl tracking-wide'>LAMA</div>
         </Link>
       <Menu/>
       </div>
       {/* BIGGER SCREENS */}
-      <div className=' hidden md:flex items-center justify-between gap-8 h-full'>
+      <div className='hidden md:flex items-center justify-between gap-8 h-full'>
         {/* LEFT */}
         <div className=' w-1/3 xl:w-1/2 flex items-center gap-12'>
 <Link href='/' className=' flex items-center gap-3'>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -32,12 +32,13 @@ const Slider = () => {
      const [current, setcurrent] = useState(0);
   return (
     <div className=' h-[calc(100vh-80px)] overflow-hidden'>
-      <div className=' w-max h-full flex transition-all ease-in-out duration-1000'>
+      <div className=' w-max h-full flex transition-all ease-in-out duration-1000'
+      style={{ transform: `translateX(-${current * 100}vw)`}}>
         {slides.map((slide)=>(
             <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
             key={slide.id}>
                 {/* TEXT CONTAINER */}
-                <div className=' h-1/2 xl:w-1/2  xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center'>
+                <div className=' h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center'>
                     <h2 className=' text-xl lg:text-3xl 2xl:text-5xl'>{slide.description}</h2>
                     <h1 className=' text-5xl lg:text-6xl 2xl:text-8xl font-semibold'>{slide.title}</h1>
                     <Link href={slide.url}>
@@ -45,30 +46,29 @@ const Slider = () => {
                     </Link>
                 </div>
                 {/* IMAGE CONTAINER */}
-                <div className=' xl:w-1/2 xl:h-full relative'>
+                <div className=' h-1/2 xl:w-1/2 xl:h-full relative'>
                     <img src={slide.img} 
                     alt="" 
-                    
                     sizes='100%' 
                     className=' h-[40rem] object-contain'/>
                 </div>
             </div>
         ))}
       </div>
-      <div>
-       {slides.map((slide,index =>(
-            <div 
-            className={'w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
-                current === index ? "scale-150" : ""
-            }'}
-             key={slide.id}
-             >
-                {current === index && (
-                    <div className=' w-[6px] h-[6px] bg-gray-600 rounded-full'></div>
-                )}
-             </div>
-        ))
-      }
+      <div className=' absolute m-auto left-1/2 bottom-8 flex gap-4'>
+      {
+        slides.map((slide,index) => (
+       <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+        current === index ? "scale-150" : ""
+       }`} key={slide.id}
+       onClick={() => setcurrent(index)}
+       >
+     {current === index && (
+        <div className=' w-[6px] h-[6px] bg-gray-600 rounded-full'></div>
+     )}
+       </div>
+        
+      ))}
       </div>
     </div>
   );
